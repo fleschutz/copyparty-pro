6bf53a9f08f5d46f2398d47b9f7f7e575a390e84
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-05-14 19:18:58 +0200

    Added run.ps1

---
+++ run.ps1
@@ -0,0 +1,4 @@
+
+"Starting 'copyparty' with white-gold theme..."
+
+python3 ./copyparty-en.py --html-head '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">' --js-browser theme/white-gold.js --css-browser theme/white-gold.css
